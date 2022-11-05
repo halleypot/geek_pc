3e127cce08cd1701818043a86ed883f9dce26aca
halleypot <halleypot@163.com>
2022-11-06 00:01:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import NoMatch from './pages/404'
+import Layout from './pages/layout'
+import Login from './pages/login'
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Redirect from='/' to='/home'></Redirect>
+          <Route path={'/home'} component={Layout}></Route>
+          <Route path={'/login'} component={Login}></Route>
+          <Route component={NoMatch}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/layout/index.js
@@ -0,0 +1,9 @@
+function Layout () {
+  return (
+    <div classname={'layout'}>
+      <h1>Layout</h1>
+    </div>
+  )
+}
+
+export default Layout
